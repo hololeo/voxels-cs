095634b0f705893f0f3e7974db6fce36224bf12a
Marc Le Bouthillier <lemarcuspoilus@users.noreply.github.com>
2017-10-25 15:25:47 -0400

    Simple game loop added, with delta and FPS computations

--- Program.cs
+++ Program.cs
@@ -1,36 +1,70 @@
 ﻿using System;
+using System.CodeDom;
+using System.Diagnostics;
+using System.Threading.Tasks;
 using OpenGL;
 
 namespace Voxels {
-    public static class Program {
-        public static void Main(string[] args) {
+    public class Program : IDisposable {
+        private IntPtr _window;
+
+        private void Init() {
             Glfw.SetErrorCallback((_, desc) => throw new Exception($"GLFW error: {desc}"));
             Glfw.Init();
 
             Glfw.WindowHint(WindowHint.Resizable, 0);
             Glfw.WindowHint(WindowHint.Visible, 0);
-            Glfw.WindowHint(WindowHint.OpenGLProfile, (int)OpenGLProfile.Core);
+            Glfw.WindowHint(WindowHint.OpenGLProfile,
+                (int) OpenGLProfile.Core);
             Glfw.WindowHint(WindowHint.ContextVersionMajor, 4);
             Glfw.WindowHint(WindowHint.ContextVersionMinor, 3);
-            var window = Glfw.CreateWindow(800, 450, "My Window", IntPtr.Zero, IntPtr.Zero);
+            _window = Glfw.CreateWindow(800, 450, "My Window", IntPtr.Zero,
+                IntPtr.Zero);
             Gl.Initialize();
-            Glfw.MakeContextCurrent(window);
-            Glfw.ShowWindow(window);
+            Glfw.MakeContextCurrent(_window);
+            Glfw.ShowWindow(_window);
+        }
 
+        public void Dispose() {
+            Glfw.DestroyWindow(_window);
+            Glfw.Terminate();
+        }
+
+        private void Run() {
             Console.WriteLine("OpenGL Version: " + Gl.CurrentVersion);
             Console.WriteLine("OpenGL Vendor: " + Gl.CurrentVendor);
             Console.WriteLine("OpenGL Renderer: " + Gl.CurrentRenderer);
 
-            while (!Glfw.WindowShouldClose(window)) {
+            var timewatch = new Stopwatch();
+            var framewatch = new Stopwatch();
+            timewatch.Start();
+            framewatch.Start();
+            var lastTime = timewatch.Elapsed;
+            var fps = 0;
+
+            while (!Glfw.WindowShouldClose(_window)) {
+                var delta = (float) ((timewatch.Elapsed - lastTime).Ticks / (double) TimeSpan.TicksPerSecond);
+                lastTime = timewatch.Elapsed;
+
                 Gl.ClearColor(1.0f, 0.5f, 0.2f, 1.0f);
                 Gl.Clear(ClearBufferMask.ColorBufferBit);
                 Glfw.SwapInterval(1);
-                Glfw.SwapBuffers(window);
+                Glfw.SwapBuffers(_window);
                 Glfw.PollEvents();
-            }
+                fps++;
 
-            Glfw.DestroyWindow(window);
-            Glfw.Terminate();
+                if (framewatch.ElapsedMilliseconds < 1000) continue;
+                Console.WriteLine($"{fps} fps");
+                framewatch.Restart();
+                fps = 0;
+            }
+        }
+
+        public static void Main(string[] args) {
+            using (var program = new Program()) {
+                program.Init();
+                program.Run();
+            }
         }
     }
 }
